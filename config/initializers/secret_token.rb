6cb4c965eaf1b245fc6df757796ced6d93f914eb
rajuadla64 <ard.bujji64@gmail.com>
2013-04-03 12:17:10 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeviseApp::Application.config.secret_token = '7f9f2193068e89527396e9ba5cc8efe0e58a0c6d05bab6ced0f7520a0a999a250597a74521fe3f72122e7c983a7e1cf3aac3adf0aec395ab7f807027de63886d'
---
+++ config/routes.rb
@@ -0,0 +1,80 @@
+DeviseApp::Application.routes.draw do
+  devise_for :users
+
+  resources :dashboard
+  #resources :password
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
+  #       post 'toggle'
+  #     end
+  #
+  #     collection do
+  #       get 'sold'
+  #     end
+  #   end
+
+  # Sample resource route with sub-resources:
+  #   resources :products do
+  #     resources :comments, :sales
+  #     resource :seller
+  #   end
+
+  # Sample resource route with more complex sub-resources
+  #   resources :products do
+  #     resources :comments
+  #     resources :sales do
+  #       get 'recent', :on => :collection
+  #     end
+  #   end
+
+  # Sample resource route within a namespace:
+  #   namespace :admin do
+  #     # Directs /admin/products/* to Admin::ProductsController
+  #     # (app/controllers/admin/products_controller.rb)
+  #     resources :products
+  #   end
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.bak.
+  root :to => 'home#index'
+  match 'home/homepage' => 'home#homepage'
+  match 'searching/searching' => 'searching#searching'
+  match  'dashboard/index' => 'dashboard#index'
+  match 'dashboard/addcourse' => 'dashboard#addcourse'
+  match 'searching/tutoring' => "searching#tutoring"
+  match 'searching/online' => "searching#online"
+  match 'admin/admin' => "admin#admin"
+  match 'vendor/vendor' => "vendor#vendor"
+  match 'learner/learner' => "learner#learner"
+  match 'searching/all' => 'searching#all'
+  match 'vendor/onlinecourse' => "vendor#onlinecourse"
+  match 'vendor/tutoringcourse' => "vendor#tutoringcourse"
+  match 'vendor/seminor' => "vendor#seminor"
+  match 'vendor/workshop' => "vendor#workshop"
+  match 'vendor/institute' => "vendor#institute"
+  match 'vendor/onlinecourse1' => "vendor#onlinecourse1"
+
+
+  # See how all your routes lay out with "rake routes"
+
+  # This is a legacy wild controller route that's not recommended for RESTful applications.
+  # Note: This route will make all actions in every controller accessible via GET requests.
+  # match ':controller(/:action(/:id))(.:format)'
+end
